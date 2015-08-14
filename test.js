cf58ac420a57bed406b38a71f6ce0ff47d51eaee
Visnu Pitiyanuvath <visnupx@gmail.com>
2015-08-14 00:07:46 -0700

    play against self a lot more

--- test.js
+++ test.js
@@ -10,25 +10,29 @@ console.log(AI.best([
 process.exit();
 */
 
-var board = [
-  [ null, null, null ],
-  [ null, null, null ],
-  [ null, null, null ],
-];
-
 var players = {
-  'x': AI.best,
+  'x': AI.random,
   'o': AI.best,
 };
-var player = 'x',
-    result;
-while (!(result = AI.end(board))) {
-  var move = players[player](board, player);
-  board[move[0]][move[1]] = player;
-  console.log(toString(board));
-  player = player === 'x' ? 'o' : 'x';
+var results = {};
+for (var i = 0; i < 1000; i++) {
+  var player = 'x',
+      board = [
+        [ null, null, null ],
+        [ null, null, null ],
+        [ null, null, null ],
+      ],
+      result;
+  while (!(result = AI.end(board))) {
+    var move = players[player](board, player);
+    board[move[0]][move[1]] = player;
+    //console.log(toString(board));
+    player = player === 'x' ? 'o' : 'x';
+  }
+  //console.log(result);
+  results[result] = (results[result] || 0) + 1;
 }
-console.log(result);
+console.log(results);
 
 function toString(board) {
   return _.map(board, function(row) {
